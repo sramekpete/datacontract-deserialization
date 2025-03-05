55ec34f424897d6463cdda2ce0c93c33f38914c1
Petr Sramek <petr.sramek@outlook.cz>
2025-03-05 20:21:48 +0100

    added program output to console

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace DataContractDeserialization;
 
 using DataContractDeserialization.Data;
+using System;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Xml;
@@ -11,8 +12,16 @@ class Program {
 
         using (var file = File.OpenRead("./Message.xml")) {
             using (var xml = XmlReader.Create(file)) {
-                var result = serializer.ReadObject(xml);
+                ApplicationRequestImpl result = (ApplicationRequestImpl)serializer.ReadObject(xml);
+
+                Console.WriteLine($"Amount is {result.Application.Amount}.");
+
+                for (int i = 0; i < result.Application_SAList.Length; i++) {
+                    Console.WriteLine($"Security key at position {i} is {result.Application_SAList[i].Security_Key}.");
+                }
             }
         }
+
+        Console.ReadKey();
     }
 }
