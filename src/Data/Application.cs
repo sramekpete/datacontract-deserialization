4c0a5ac45f784beafad4336f81ce21201bb15d2e
Pete Sramek <petr.sramek@dropoutcoder.com>
2025-03-05 20:24:07 +0100

    Framework update (#1)
    * source code moved to root folder
    
    * refactored, updated, cleaned up
    
    * updated readme
    
    * added program output to console
    
    ---------
    
    Co-authored-by: Petr Sramek <petr.sramek@outlook.cz>

---
+++ src/Data/Application.cs
@@ -0,0 +1,9 @@
+﻿namespace DataContractDeserialization.Data;
+
+using System.Runtime.Serialization;
+
+[DataContract(Namespace = "http://xyz")]
+public class Application {
+    [DataMember]
+    public string Amount;
+}
---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DataContractDeserialization;
+
+using DataContractDeserialization.Data;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+class Program {
+    static void Main(string[] _) {
+        var serializer = new DataContractSerializer(typeof(ApplicationRequestImpl));
+
+        using (var file = File.OpenRead("./Message.xml")) {
+            using (var xml = XmlReader.Create(file)) {
+                ApplicationRequestImpl result = (ApplicationRequestImpl)serializer.ReadObject(xml);
+
+                Console.WriteLine($"Amount is {result.Application.Amount}.");
+
+                for (int i = 0; i < result.Application_SAList.Length; i++) {
+                    Console.WriteLine($"Security key at position {i} is {result.Application_SAList[i].Security_Key}.");
+                }
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
