f9647bfe4a042ad2f677d9425e15b2911728f82f
Petr Sramek <petr.sramek@outlook.cz>
2025-03-05 19:25:16 +0100

    refactored, updated, cleaned up

---
+++ src/Data/ApplicationRequestImpl.cs
@@ -0,0 +1,12 @@
+﻿namespace DataContractDeserialization.Data;
+
+using System.Runtime.Serialization;
+
+[DataContract(Namespace = "http://xyz")]
+public class ApplicationRequestImpl {
+    [DataMember]
+    public Application Application;
+
+    [DataMember]
+    public Application_SA[] Application_SAList;
+}
---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DataContractDeserialization;
+
+using DataContractDeserialization.Data;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml;
+
+class Program {
+    static void Main(string[] _) {
+        var serializer = new DataContractSerializer(typeof(ApplicationRequestImpl));
+
+        using (var file = File.OpenRead("./Message.xml")) {
+            using (var xml = XmlReader.Create(file)) {
+                var result = serializer.ReadObject(xml);
+            }
+        }
+    }
+}
